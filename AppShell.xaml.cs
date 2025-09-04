5bf6febc6c3a0fe651322e9f1f22b467111bac0a
tommirp <tommirp@gmail.com>
2025-09-04 15:16:22 +0700

    ok

---
+++ AppShell.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Maui.Controls;
+
+#if ANDROID
+using Android.OS;
+using Android.Views;
+using Microsoft.Maui.Platform;
+#endif
+
+#if IOS
+using UIKit;
+using CoreGraphics;
+#endif
+
+namespace PASMAN
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            SetStatusBarBlack();
+        }
+        private void SetStatusBarBlack()
+        {
+#if ANDROID
+        var activity = Platform.CurrentActivity;
+
+        if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+        {
+            // Set Android status bar background to black
+            activity.Window.SetStatusBarColor(Android.Graphics.Color.Black);
+
+            // Light icons (white)
+            activity.Window.DecorView.SystemUiVisibility = 0;
+        }
+#endif
+
+#if IOS
+        // Set iOS status bar style to light (white text/icons)
+        UIApplication.SharedApplication.StatusBarStyle = UIStatusBarStyle.LightContent;
+
+        // Optional: black background behind status bar
+        var statusBarFrame = UIApplication.SharedApplication.StatusBarFrame;
+        var statusBarView = new UIView(statusBarFrame)
+        {
+            BackgroundColor = UIColor.Black
+        };
+
+        // Add the view to the main window
+        UIApplication.SharedApplication.KeyWindow?.AddSubview(statusBarView);
+#endif
+        }
+    }
+}
